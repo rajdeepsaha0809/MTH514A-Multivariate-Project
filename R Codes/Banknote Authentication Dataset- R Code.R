276f0eed357af6370e6a7b71099b11f5752c0bb9
Rajdeep Saha <86014455+rajdeepsaha0809@users.noreply.github.com>
2022-04-20 02:42:17 +0530

    Add files via upload

---
+++ R Codes/Banknote Authentication Dataset- R Code.R
@@ -0,0 +1,112 @@
+#Loading the Data
+
+rm(list=ls())
+set.seed(201380)
+data<- read.csv("https://raw.githubusercontent.com/rajdeepsaha0809/MTH514A-Multivariate-Project/main/BankNote_Authentication.csv")
+head(data)
+dim(data)
+str(data)
+data$class <- as.factor(data$class)
+#Checking for missing values
+sum(is.na(data))
+#Check for Data Imbalance
+attach(data)
+sum(class == 1) / nrow(data)
+sum(class == 0) / nrow(data)
+#Data Split
+names(data)
+index= sample(1:nrow(data),floor(0.85*nrow(data)))
+train= data[index, ]
+remaining= data[-index, ]
+index2 = sample(1:nrow(remaining),floor(2/3*nrow(remaining)))
+crossval= remaining[index2, ]
+test = remaining[-index2, ]
+actual_class=crossval$class
+dim(train)
+dim(crossval)
+dim(test)
+library(tibble)
+library(cvms)
+f_cfm <- function(x){
+  cfm <- as.tibble(x)
+  cname <- colnames(cfm)
+  print(plot_confusion_matrix(cfm, target_col = cname[2], prediction_col =  cname[1], counts_col = cname[3]))
+}
+#Logistic Regression
+attach(data)
+class <- as.factor(class)
+logistic.fit <- glm(class~., data = train, family = binomial)
+logistic.probs <- predict(logistic.fit, crossval, type = "response")
+logistic.pred <- rep(0, nrow(crossval))
+logistic.pred[logistic.probs > 0.5] = 1
+actual <- crossval$class
+logistic_table <- table(logistic.pred, actual)
+f_cfm(logistic_table)
+#Normality Test
+library(Hmisc)
+hist.data.frame(data[,1:4])
+#Decision Tree
+library(tree)
+tree.fit <- tree(class~., data= train)
+cv.class <- cv.tree(tree.fit, FUN= prune.misclass)
+cv.class #dev corresponds to misclassification error rate
+par(mfrow=c(1,2))
+plot(cv.class$size, cv.class$dev, type= "b")
+plot(cv.class$k, cv.class$dev, type= "b")
+prune.tree <- prune.misclass(tree.fit, best = 8)
+plot(prune.tree)
+text(prune.tree, pretty = 0)
+tree.pred <- predict(prune.tree, crossval, type = "class")
+tree_table <- table(tree.pred, actual)
+f_cfm(tree_table)
+#Random Forest
+library(randomForest)
+used_pred <- floor(sqrt(ncol(data)-1))
+rf.fit <- randomForest(class~., data = train, mtry = used_pred,
+           importance = TRUE, maxdepth = 8)
+rf.pred <- predict(rf.fit, newdata = crossval)
+rf_table <- table(rf.pred, actual)
+f_cfm(rf_table)
+logistic_table
+tree_table
+rf_table
+F_score <- function(x){
+  p <- x[2, 2]/(x[2, 2]+ x[2, 1])
+  r <- x[2, 2]/(x[2, 2]+ x[1, 2])
+  f <- round(2*p*r/(p+r), 4)
+  f
+}
+F_score(logistic_table)
+F_score(tree_table)
+F_score(rf_table)
+paste("We have got the maximum F-Score for Random Forest which is", round(F_score(rf_table), 4))
+#Plotting ROC curve
+
+#Logistic Regression
+library(pROC)
+logistic_prob = predict(logistic.fit, newdata = crossval, type = "response")
+logistic_roc = roc(crossval$class ~ logistic_prob, plot = TRUE, main = "ROC Curve for Logistic Regression", print.auc = TRUE)
+paste("Area under the curve is",round(auc(logistic_roc), 5))
+
+#Decision Tree
+tree_predict= predict(prune.tree, crossval, type="vector")
+tree_roc = roc(crossval$class~tree_predict[,2], plot = TRUE, main = "ROC Curve for Decision Tree", print.auc = TRUE)
+paste("Area under the curve is",round(auc(tree_roc), 5))
+
+#Random Forest
+rf_predict= predict(rf.fit, crossval, type="prob")
+rf_roc = roc(crossval$class~rf_predict[,2], plot = TRUE, main = "ROC Curve for Random Forest", print.auc = TRUE)
+paste("Area under the curve is",round(auc(rf_roc), 5))
+#Final Fit
+final_fit <- predict(rf.fit, newdata = test)
+actual_test <- test$class
+final_rf_table <- table(final_fit, actual_test)
+f_cfm(final_rf_table)
+#Evaluation Metric for Test Set
+
+#F_Score
+F_score(final_rf_table)
+
+rf_predict_test= predict(rf.fit, test, type="prob")
+tree_roc_test = roc(test$class~rf_predict_test[,2], plot = TRUE, main="ROC Curve for Final Model", print.auc = TRUE)
+paste("Area under the curve is",round(auc(tree_roc_test),4))
